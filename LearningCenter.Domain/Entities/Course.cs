e5b8f232b99d3837bfc867fee221d4922a0cf108
Temurkhann <temurxonshodiyev4@gmail.com>
2023-11-08 19:21:22 +0500

    Course entity created

---
+++ LearningCenter.Domain/Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿using LearningCenter.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCenter.Domain.Entities
+{
+    public class Course : Auditable
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
