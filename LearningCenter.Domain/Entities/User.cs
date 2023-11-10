c35eb150e3e29beac59c14da6795876a53918c30
Azizbek <adkhamovaz26@gmail.com>
2023-11-10 19:12:43 +0500

    DbContexts and Repositories were added

--- LearningCenter.Domain/Entities/User.cs
+++ LearningCenter.Domain/Entities/User.cs
@@ -12,6 +12,4 @@ public class User : Auditable
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
-
-    public DateTime Created { get; set; }   
 }
